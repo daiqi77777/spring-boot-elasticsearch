30f9052311edf52cfc9e0fb8270d23e5602c57c2
小柒2012 <345849402@qq.com>
2018-01-29 13:51:32 +0800

    新增Dubbo打包接口

--- src/main/java/com/itstyle/es/Application.java
+++ src/main/java/com/itstyle/es/Application.java
@@ -2,6 +2,7 @@ package com.itstyle.es;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 /**
  * 启动类
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * linux 下 后台启动  nohup java -jar acts_elasticsearch.jar --server.port=8886 & 
  */
 @SpringBootApplication
+//不想使用可以注释掉
+@ImportResource({"classpath:spring-context-dubbo.xml"})
 public class Application extends WebMvcConfigurerAdapter {
 	private static final Logger logger = Logger.getLogger(Application.class);
 	
