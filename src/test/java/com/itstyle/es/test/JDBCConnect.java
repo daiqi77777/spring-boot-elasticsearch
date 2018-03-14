01560fccbd2b462c363f8726230504a33e4729d8
小柒2012 <345849402@qq.com>
2018-03-14 10:42:15 +0800

    更新 JDBCConnect.java

--- src/test/java/com/itstyle/es/test/JDBCConnect.java
+++ src/test/java/com/itstyle/es/test/JDBCConnect.java
@@ -1,22 +1,10 @@
-/**
- * Copyright (c) 2010-2013 by ACTS
- * All rights reserved.
- */
+
 package com.itstyle.es.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *@Function: TODO
- *@Class Name: test
- *@Author: lxz
- *@Date: 2013-7-11
- *@Modifications:
- *@Modifier Name; Date; The Reason for Modifying
- *
- */
 public class JDBCConnect {
 
 	private static Connection connection;
